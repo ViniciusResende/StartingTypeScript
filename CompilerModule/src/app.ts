d5aeec529ff2a8650a3000267208b8dfa0405944
Vinícius Alves <viniciusfariaresende@Gmail.com>
2021-02-16 18:46:12 -0300

    Finishing the firt part of the course

---
+++ CompilerModule/src/app.ts
@@ -0,0 +1,10 @@
+// tsc app.ts -w || tsc app.ts --watch
+const button = document.querySelector('button')!;
+
+function clickHandler(message: string){
+  console.log('Clicked! ' + message);
+}
+
+if(button){
+  button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
+}
